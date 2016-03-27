3adc8af031fd2ce76bca81aa7f5ff065baa92335
MattGauler <matt.gauler@gmail.com>
2016-03-27 16:17:03 +0100

    GenerateSupportMessage

--- managers/messageManager.js
+++ managers/messageManager.js
@@ -1,54 +1,63 @@
 "use strict";
 var Guid = require('guid');
+var moment = require('moment');
 var MsgManager = (function () {
     function MsgManager() {
         this.baseMsg = this.generateBaseMessage();
     }
     MsgManager.prototype.generateBaseMessage = function () {
         /*
+        
         {
-            "channel": "t-notifyshamrock-0", 
+            "id": "c56bcaff-8e00-446c-8755-9c477b742db7",
+            "channel": "t-notifyshamrock-0",
+            "subchannel": "1234",
+            "expiry": 1457607834463,
+            "date": 1457604234466,
+            "notification": "",
+            "provider": "",
             "content": {
-                "@class": "ConnectivityRequest", 
+                "@class": "ConnectivityRequest",
                 "data": {
-                "requestExpiry": 1457604299655
-                }, 
-                "driverId": "53224eb8-ce59-42a8-9391-e35d7a5b9eb5", 
-                "id": "cff50718-c3eb-f2ed-45bc-fe662fd102f8", 
-                "messageDate": "2016-03-10T10:03:19+00:00", 
+                    "requestExpiry": 1457604299655
+                },
+                "driverId": "53224eb8-ce59-42a8-9391-e35d7a5b9eb5",
+                "id": "cff50718-c3eb-f2ed-45bc-fe662fd102f8",
+                "messageDate": "2016-03-10T10:03:19+00:00",
                 "subscriptionToken": "5-20501-6a996228-d73b-1add-baad-8c04bf8cbccb"
-            }, 
-            "date": 1457604234466, 
-            "expiry": 1457607834463, 
-            "id": "c56bcaff-8e00-446c-8755-9c477b742db7", 
-            "notification": "", 
-            "provider": "", 
-            "subchannel": "1234"
+            }
         }
         */
-        
         var message = {
             id: Guid.raw(),
-            channel: '899991',
-            subchannel: '1',
-            expiry: 0,
-            date: 131007203230000000,
+            channel: 't-notifyshamrock-0',
+            subchannel: '1234',
+            expiry: moment().add(1, 'm').valueOf(),
+            date: moment().valueOf(),
+            notification: '',
+            provider: '',
             content: {
                 '@class': ''
             }
         };
-        
         return message;
-        
     };
     MsgManager.prototype.generateConnectionRequest = function (userId) {
         var base = this.generateBaseMessage();
-        var msgData = {};
-        msgData.requestExpiry = moment()
-        
         var msg = base;
-        msg.content['@class'] = 'ConnectivityRequest';
-        msg.content.data = msgData;
+        var content = {
+            content: {
+                "@class": "ConnectivityRequest",
+                data: {
+                    "requestExpiry": moment().add(1, 'm').valueOf()
+                },
+                driverId: "",
+                id: Guid.raw(),
+                messageDate: moment().toDate(),
+                subscriptionToken: "5-20501-6a996228-d73b-1add-baad-8c04bf8cbccb"
+            }
+        };
+        msg.content = content;
         return msg;
     };
     return MsgManager;
--- managers/messageManager.ts
+++ managers/messageManager.ts
@@ -1,4 +1,5 @@
 var Guid = require('guid');
+var moment = require('moment');
     
 export class MsgManager {
     
@@ -13,29 +14,61 @@ export class MsgManager {
     
     generateBaseMessage(): any {
         
-        var customProperties = {
+        /*
+        
+        {
+            "id": "c56bcaff-8e00-446c-8755-9c477b742db7", 
+            "channel": "t-notifyshamrock-0", 
+            "subchannel": "1234",
+            "expiry": 1457607834463, 
+            "date": 1457604234466,
+            "notification": "", 
+            "provider": "", 
+            "content": {
+                "@class": "ConnectivityRequest", 
+                "data": {
+                    "requestExpiry": 1457604299655
+                }, 
+                "driverId": "53224eb8-ce59-42a8-9391-e35d7a5b9eb5", 
+                "id": "cff50718-c3eb-f2ed-45bc-fe662fd102f8", 
+                "messageDate": "2016-03-10T10:03:19+00:00", 
+                "subscriptionToken": "5-20501-6a996228-d73b-1add-baad-8c04bf8cbccb"
+            }             
+        }
+        */
+        
+        var message = {
             id: Guid.raw(),
-            channel: '899991',
-            subchannel: '1',
-            expiry: 0,
-            date: 131007203230000000,
+            channel: 't-notifyshamrock-0',
+            subchannel: '1234',
+            expiry: moment().add(1,'m').valueOf(),
+            date: moment().valueOf(),
+            notification: '', 
+            provider: '', 
             content: {
                 '@class': ''
             }
         };
 
-        var message = {
-            body: JSON.stringify(customProperties),
-            customProperties: customProperties
-        };
-        
-        
         return message;
     }
     
     generateConnectionRequest(userId:string): any{
         var base = this.generateBaseMessage();
         var msg = base;
+        var content = {
+            content: {
+                "@class": "ConnectivityRequest", 
+                data: {
+                    "requestExpiry": moment().add(1,'m').valueOf()
+                }, 
+                driverId: "", 
+                id: Guid.raw(), 
+                messageDate: moment().toDate(), 
+                subscriptionToken: "5-20501-6a996228-d73b-1add-baad-8c04bf8cbccb"
+            }
+        }
+        msg.content = content;
         return msg;
     }
     
